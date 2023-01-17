4204eddc309d0650a1786b00834fa657ea573aa5
FZsiga02 <fzsigmond02@gmail.com>
2023-01-17 14:45:40 +0100

    Beolvasás

--- src/main/java/com/example/konyvtarasztali/Statisztika.java
+++ src/main/java/com/example/konyvtarasztali/Statisztika.java
@@ -1,4 +1,17 @@
 package com.example.konyvtarasztali;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Statisztika {
+    private static List<Konyv> konyvek;
+
+    public static void main(String[] args) {
+        System.out.println("Statisztika");
+    }
+
+    private void Beolvas() {
+        konyvek = new ArrayList<>();
+
+    }
 }
